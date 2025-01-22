13f718505020fbb12409f4f0b098a0a1a32123c6
Fady Abousifein <fadyasifein@gmail.com>
2025-01-22 16:55:00 -0500

    q4 done, TODO:comment

--- Assignment1/400506836-asg1-4.cpp
+++ Assignment1/400506836-asg1-4.cpp
@@ -10,24 +10,38 @@ class AbsProgression {
         AbsProgression() {
             first = 2; 
             second = 200; 
-
-            for (int i = 0; i < 8; i++) {
-                
-            }
+            printProgression(10);
         }
 
         AbsProgression(int first, int second) {
             this->first = first;
             this->second = second; 
+            printProgression(10);
         }
 
-        int nextNum(int first, int second) {
-            return abs(second - first); 
+        int nextNum() {
+            int nextNum = abs(second - first); 
+            first = second; 
+            second = nextNum; 
+            return nextNum; 
+        }
+
+        void printProgression(int n) {
+            std::cout << first << " " << second << " "; 
+            for (int i = 0; i < 8; i++) {
+                std::cout << nextNum() << " "; 
+            }
         }
 
 
 }; 
 
 int main() {
+    std::cout << "Absolute progression with default constructor:" << std::endl;
+    AbsProgression defaultProgression;  // Should output 10 values starting from 2 and 200
+
+    std::cout << std::endl << std::endl << "Absolute progression with custom constructor:" << std::endl;
+    AbsProgression customProgression(300, 297);
+
     return EXIT_SUCCESS; 
 }
