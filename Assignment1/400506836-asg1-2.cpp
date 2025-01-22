27bb2732f27b1f3aab41ef5b6aafe106647dac70
Fady Abousifein <fadyasifein@gmail.com>
2025-01-22 11:40:49 -0500

    question2 commented

--- Assignment1/400506836-asg1-2.cpp
+++ Assignment1/400506836-asg1-2.cpp
@@ -14,6 +14,7 @@ class Birthday {
             month = (rand() % 12) + 1; // 1 - 12 
         }
 
+        // overloads == operator to check whether birthdays are equal
         bool operator == (Birthday * birthday) {
             if (day == birthday->day && month == birthday-> month ) {
                 return true;  
@@ -26,6 +27,7 @@ class BirthdayParadox {
         Birthday *birthdays; // Array for holding birthdays
         const int num_people; // Number of birthdays to generate
         
+        // constructor creates n birthdays with a random date
         BirthdayParadox(int n) : num_people(n) {
             birthdays = new Birthday[num_people]; 
             
@@ -39,10 +41,11 @@ class BirthdayParadox {
             delete [] birthdays; 
         } 
         
+        // checks whether any two of the n birthdays are the same
         int CheckDuplicates(Birthday * birthdays, int n) {
             for (int i = 0; i < n; i++) {
                 for (int j = i + 1; j < n; j++) {
-                    if (&(birthdays[i]) == &(birthdays[j])) {
+                    if (birthdays[i] == &(birthdays[j])) {
                         return 1;  
                     } 
                 }
@@ -54,18 +57,21 @@ class BirthdayParadox {
 int main() {
     srand(time(0)); 
 
+    // runs test cases for n = 5, 10, 15, .. , 100
     for (int n = 5; n <= 100; n += 5) {
         int dupes = 0; 
 
+        // for each n, 100 experiments are ran in which new random birthdays are generated 
         for (int i = 0; i < 100; i++) {
             BirthdayParadox bParadox = BirthdayParadox(n); 
             
+            // checks for duplicate birthdays
             if (bParadox.CheckDuplicates(bParadox.birthdays, n)) {
                 dupes++; 
             }
         }
         
-        std::cout << n << "," << dupes << "," <<  dupes/100 << std::endl; 
+        std::cout << n << "," << dupes << "," <<  dupes/100. << std::endl; 
     }
     return EXIT_SUCCESS; 
 }
